96a034bfba969ecab7336043e391ab91dc07b186
Cojocari Stanislav <stasiccojocari78@gmail.com>
2026-04-13 19:44:28 +0300

    feat: add IUserService interface for profile management

---
+++ TaskManagementApp/TaskManagement.BusinessLayer/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Http;
+using TaskManagement.Domain.Models.Auth;
+using TaskManagement.Domain.Models.Users;
+
+namespace TaskManagement.BusinessLayer.Interfaces;
+
+public interface IUserService
+{
+    Task<PublicUserProfileResponse> GetPublicProfileAsync(Guid targetUserId, Guid requestingUserId);
+    Task<string> UploadAvatarAsync(Guid userId, IFormFile file, string webRootPath);
+    Task DeleteAccountAsync(Guid userId);
+    Task<AuthResponse> UpdateProfileAsync(Guid userId, UpdateProfileRequest request);
+}
